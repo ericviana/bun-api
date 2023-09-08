79e50f3c4483d03151f8b5cc3730f4ddcacb613e
Eric Viana <ericvsdefault@gmail.com>
2023-09-08 18:39:36 -0300

    Guard

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,37 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .group("/search", (app) => {
+    return app
+      .guard({
+        schema: {
+          query: t.Object({
+            q: t.String()
+          })
+        }
+      })
+      .get("/", ({ query }) => `query: ${query.q}`)
+      .get("/movie", ({ query }) => `query: ${query.q}`)
+      .get("/tv", ({ query }) => `query: ${query.q}`)
+      .get("/person", ({ query }) => `query: ${query.q}`)
+      .get("/company", ({ query }) => `query: ${query.q}`)
+      .get("/episode", ({ query }) => `query: ${query.q}`)
+      .get("/review", ({ query }) => `query: ${query.q}`)
+      .get("/award", ({ query }) => `query: ${query.q}`)
+  })
+
+  .group("/title/:id", (app) => {
+    return app
+      .get("/", ({ params }) => `id: ${params.id}`)
+      .get("/episodes", ({ params }) => `id: ${params.id}`)
+      .get("/cast", ({ params }) => `id: ${params.id}`)
+      .get("/reviews", ({ params }) => `id: ${params.id}`)
+      .get("/awards", ({ params }) => `id: ${params.id}`)
+  })
+
+  
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+)
